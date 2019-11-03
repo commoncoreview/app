4e3fb1765cb0534f84b25b7d3340edf567465dc4
Jeffrey Baumes <jeff.baumes@kitware.com>
2019-11-03 09:59:03 -0500

    Fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/app/'
+    : '/',
   outputDir: path.resolve(__dirname, './docs'),
   transpileDependencies: [
     'vuetify'
